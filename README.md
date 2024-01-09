249e30e31a368a4e2b6ebea5a7e988abda091bca
WasinUddy <wasinsilakong@gmail.com>
2024-01-09 09:52:40 +0700

    add thick version

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# Montainer
-Minecraft in a container
+<p align="center">
+  <img src="https://github.com/WasinUddy/Montainer/blob/main/resources/logo.png?raw=true" alt="Montainer Logo" width="200"/>
+</p>
+
+---
+
+### Minecraft + Container = Montainer !!!
+
+Since in order to play Minecraft Bedrock Edition the client must be the same version as the server, updating the client is super easy via the Microsoft Store, but updating the server is super complicated since it requires downloading a new binary and replacing the old one. This project puts the server in a container so that it can be updated easily by mounting the world and configuration files as volumes and easily updating the container.
+
+
+## Features
+1. Containerized Minecraft Bedrock Edition Server
+2. Web UI for managing the server no need to interact with the container directly
+3. Self Managing Repositories automatically update the container when a new version is released by scraping from the Mojang website
+4. Thick image version which contained the server inside the image
+
+## Usage
+There are two ways to use this project, either run it directly from docker or use the docker-compose which is the recommended way. You may use it with services like Watchtower to automatically update the container when a new version is released so you can enjoy minecraft without worrying about updating the server.
+
+### Sample docker-compose.yaml
+
+```yaml
+version: "3.8"
+
+services:
+  montainer:
+    image: ghcr.io/wasinuddy/montainer:latest
+    container_name: montainer
+    restart: unless-stopped
+    ports:
+      - 19132:19132/udp
+      - 8000:8000
+    volumes:
+      - ./allowlist.json:/minecraft/minecraft_server/allowlist.json
+      - ./server.properties:/minecraft/minecraft_server/server.properties
+      - ./level:/minecraft/minecraft_server/worlds/Bedrock level
+```
+Access the Web UI via http://localhost:8000 and game via localhost:19132
+
+## TODO
+-  VMWare vCenter inspired vTainer server to cluster multiple server 
+-  Add support for preview versions
+
--- scripts/server_downloader.py
+++ scripts/server_downloader.py
@@ -4,29 +4,39 @@ import argparse
 
 
 def main():
-    # Load versions file
-    with open('/minecraft/scripts/versions.json') as f:
-        versions = json.load(f)
 
-
-    parser = argparse.ArgumentParser(description='Download Minecraft Bedrock Server from Mojang server.')
-    parser.add_argument('--type', type=str, help='Specify the server type (stable or preview).')
+    parser = argparse.ArgumentParser(description="Download Minecraft Bedrock Server from Mojang server.")
+    parser.add_argument("--type", type=str, help="Specify the server type (stable or preview).")
+    parser.add_argument("--path", type=str, help="Specify the path to the server directory.")
 
     args = parser.parse_args()
 
-    if args.type=='stable':
-        url = f'https://minecraft.azureedge.net/bin-linux/bedrock-server-{versions["stable"]}.zip'
+    # Set Path
+    if args.path:
+        path = args.path
     else:
-        url = f'https://minecraft.azureedge.net/bin-linux-preview/bedrock-server-{versions["preview"]}.zip'
+        path = "/minecraft"
+
+    # Load versions file
+    with open(f"{path}/versions.json") as f:
+        versions = json.load(f)
+
     
 
-    print(f'Downloading {url}...')
-    os.system("wget -O /minecraft/tmp/bedrock-server.zip " + url)
-    print('Extracting...')
-    os.system("unzip /minecraft/tmp/bedrock-server.zip -d /minecraft/minecraft_server")
-    print('Cleaning up...')
-    os.system("rm /minecraft/tmp/bedrock-server.zip")
+    if args.type=="stable":
+        url = f"https://minecraft.azureedge.net/bin-linux/bedrock-server-{versions['stable']}.zip"
+    else:
+        url = f"https://minecraft.azureedge.net/bin-linux-preview/bedrock-server-{versions['preview']}.zip"
+    
+    print(f"Downloading {url}...")
+    os.system(f"mkdir -p {path}/tmp")
+    os.system(f"wget -O {path}/tmp/bedrock-server.zip " + url)
+    print("Extracting...")
+    os.system(f"unzip {path}/tmp/bedrock-server.zip -d {path}/minecraft_server")
+    print("Cleaning up...")
+    os.system(f"rm {path}/tmp/bedrock-server.zip")
+    
     
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
