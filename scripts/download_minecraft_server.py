27492eee50c41fbe381600d6ab90f56f296337cf
WasinUddy <wasinsilakong@gmail.com>
2024-03-06 23:12:00 +0700

    Updates server version system to multi text files

--- scripts/download_minecraft_server.py
+++ scripts/download_minecraft_server.py
@@ -12,33 +12,33 @@ def main():
     args = parser.parse_args()
 
     # Load versions file
-    with open("versions.json") as f:
-        versions = json.load(f)
+    with open(f"versions/{args.type}.txt", 'r') as f:
+        version = f.read().strip()
+        
 
     # Set URL
-    url = f"https://minecraft.azureedge.net/bin-linux{"" if args.type=="stable" else "-preview"}/bedrock-server-{versions[args.type]}.zip" if args.type in versions else None
+    url = f"https://minecraft.azureedge.net/bin-linux{"" if args.type=="stable" else "-preview"}/bedrock-server-{version}.zip"
 
-    if url:
-        print(f"Downloading {url}...")
-        response = requests.get(url)
-        response.raise_for_status()
+    # Download and extract server
+    print(f"Downloading {url}...")
+    response = requests.get(url)
+    response.raise_for_status()
 
-        print("Extracting...")
-        with zipfile.ZipFile(io.BytesIO(response.content)) as z:
-            z.extractall(os.path.join("minecraft_server"))
+    print("Extracting...")
+    with zipfile.ZipFile(io.BytesIO(response.content)) as z:
+        z.extractall(os.path.join("minecraft_server"))
 
-        print("Setting permissions...")
-        os.chmod(os.path.join("minecraft_server", "bedrock_server"), 0o777)
+    print("Setting permissions...")
+    os.chmod(os.path.join("minecraft_server", "bedrock_server"), 0o777)
 
-        # Create Base World Directory for Volume Mount
-        os.makedirs(os.path.join("minecraft_server", "worlds", "Bedrock level"), exist_ok=True)
+    # Create Base World Directory for Volume Mount
+    os.makedirs(os.path.join("minecraft_server", "worlds", "Bedrock level"), exist_ok=True)
 
-        print("Cleaning up...")
+    print("Cleaning up...")
 
 
-        print("Done.")
-    else:
-        print(f"Invalid server type: {args.type}. Please specify 'stable' or 'preview'.")
+    print("Done.")
+    
 
 if __name__ == "__main__":
     main()
